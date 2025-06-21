0495f25c05d4302be52dba32d6b58136cba3ec6e
T-Fathima <tatheerfathima2004@gmail.com>
2025-06-21 15:17:45 +0530

    few fixes

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -4,10 +4,10 @@ function Footer() {
             <span>
                 Build with ❤️ by{" "}
                 <a
-                    href="https://github.com/sahilatahar"
+                    href="https://github.com/tatheer-fathima"
                     className="text-primary underline underline-offset-1"
                 >
-                    sahilatahar
+                    Tatheer Fathima
                 </a>
             </span>
         </footer>
